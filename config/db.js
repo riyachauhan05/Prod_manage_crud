a4475b0b2f2577305f001480919b2da98cb5dbb3
riyachauhan05 <chauhanriya542003@gmail.com>
2024-10-26 10:56:08 +0530

    changed

--- config/db.js
+++ config/db.js
@@ -1,11 +1,31 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//     try {
+//         await mongoose.connect(process.env.COSMOSDB_URI);
+//         console.log('CosmosDB connected');
+//     } catch (error) {
+//         console.error('CosmosDB connection error:', error.message);
+//         process.exit(1);
+//     }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+console.log('COSMOSDB_URI:', process.env.COSMOSDB_URI); // Add this line for debugging
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.COSMOSDB_URI);
+        await mongoose.connect(process.env.COSMOSDB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('CosmosDB connected');
     } catch (error) {
         console.error('CosmosDB connection error:', error.message);
