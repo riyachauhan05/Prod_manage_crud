37711f8b5d1eda8384271738ee16c1b367489df3
riyachauhan05 <chauhanriya542003@gmail.com>
2024-10-27 20:56:52 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,25 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+
 // Registering a new user
 exports.register = async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        // Checking if the username already exists
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exist' });
+        }
+
+        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user
         const user = await User.create({ username, password: hashedPassword });
+
+        // Respond with success message
         res.status(201).json({ message: 'User registered successfully', user });
     } catch (error) {
         if (error.name === 'ValidationError') {
@@ -17,7 +29,6 @@ exports.register = async (req, res) => {
         res.status(500).json({ message: 'Error registering user', error: error.message });
     }
 };
-
 exports.login = async (req, res) => {
     const { username, password } = req.body;
 
